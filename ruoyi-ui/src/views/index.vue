63cd5b77207612524cddeb7eb61351efb73d6a2d
张宝楠 <1013271157@qq.com>
2026-03-25 15:34:24 +0800

    Initial commit

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container home">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.9.1"
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
+
---
+++ ruoyi-ui/src/views/tool/swagger/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <i-frame :src="src" />
+</template>
+<script>
+import iFrame from "@/components/iFrame/index"
+export default {
+  name: "Swagger",
+  components: { iFrame },
+  data() {
+    return {
+      src: process.env.VUE_APP_BASE_API + "/doc.html"
+    }
+  }
+}
+</script>
