73fbf1cf3a0f01eeb7e05e3628ce08cba0c5f3fc
张宝楠 <1013271157@qq.com>
2026-03-25 20:49:11 +0800

    更新logo

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -6,25 +6,22 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
-    public static void main(String[] args)
-    {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+    public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("(♥◠‿◠)ﾉﾞ  码上教学服务启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                "                      _                              _                       _             \n" +
+                "                     | |                            | |                     (_)            \n" +
+                "  _ __ ___   __ _ ___| |__   __ _ _ __   __ _    ___| | ___  __ _ _ __ _ __  _ _ __   __ _ \n" +
+                " | '_ ` _ \\ / _` / __| '_ \\ / _` | '_ \\ / _` |  / _ \\ |/ _ \\/ _` | '__| '_ \\| | '_ \\ / _` |\n" +
+                " | | | | | | (_| \\__ \\ | | | (_| | | | | (_| | |  __/ |  __/ (_| | |  | | | | | | | | (_| |\n" +
+                " |_| |_| |_|\\__,_|___/_| |_|\\__,_|_| |_|\\__, |  \\___|_|\\___|\\__,_|_|  |_| |_|_|_| |_|\\__, |\n" +
+                "                                         __/ |                                        __/ |\n" +
+                "                                        |___/                                        |___/ ");
     }
 }
