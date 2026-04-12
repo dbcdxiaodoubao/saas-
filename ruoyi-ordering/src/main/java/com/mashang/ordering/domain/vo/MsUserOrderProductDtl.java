c26ea231e8e98ccf6a839d25c89463c520820905
Koda <1838280261@qq.com>
2026-04-12 15:43:35 +0800

    修改订单详情查询接口

--- ruoyi-ordering/src/main/java/com/mashang/ordering/domain/vo/MsUserOrderProductDtl.java
+++ ruoyi-ordering/src/main/java/com/mashang/ordering/domain/vo/MsUserOrderProductDtl.java
@@ -8,24 +8,28 @@ import lombok.Data;
 @ApiModel("用户订单详情商品实体")
 public class MsUserOrderProductDtl {
 
-  @ApiModelProperty("商品id")
-  private long productId;
+  @ApiModelProperty("商品封面url")
+  private String productImage;
 
   @ApiModelProperty("商品名称")
   private String productName;
 
+  @ApiModelProperty("商品单价")
+  private Double productPrice;
+
   @ApiModelProperty("商品数量")
-  private long productQuantity;
+  private Long productQuantity;
+
+  @ApiModelProperty("出单状态（'0'待出单，'1'已出单）")
+  private String issueStatus;
 
   @ApiModelProperty("规格")
   private String specification;
 
-  @ApiModelProperty("商品价格")
-  private long productPrice;
-
   @ApiModelProperty("累计加菜次数")
-  private long cumulativeAddCount;
+  private Long cumulativeAddCount;
 
-  @ApiModelProperty("商品封面url")
-  private String productImage;
+  @ApiModelProperty("商品id")
+  private Long productId;
+
 }
